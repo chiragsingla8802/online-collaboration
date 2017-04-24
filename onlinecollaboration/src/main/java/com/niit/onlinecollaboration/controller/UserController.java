17452cc15ea341d9ae00885dc1859cdaee4c62eb
chiragsingla8802 <chirag.singla8802@gmail.com>
2017-04-24 15:56:55 +0530

    rest controller tested with postman

--- onlinecollaboration/src/main/java/com/niit/onlinecollaboration/controller/UserController.java
+++ onlinecollaboration/src/main/java/com/niit/onlinecollaboration/controller/UserController.java
@@ -23,12 +23,12 @@ public class UserController {
 	private UserDao userDao;
 
 	@RequestMapping("/get/{id}")
-	public ResponseEntity<User_Detail> get(@PathVariable int id) {
+	public ResponseEntity<User_Detail> getUserById(@PathVariable int id) {
 		System.out.println("-------------------------------------reached into controller--------------------------------------------");
 		return new ResponseEntity<User_Detail>(userDao.getUserDetail(id), HttpStatus.OK);
 	}
 
-	@PostMapping("/recieve")
+	@PostMapping("/insert")
 	public ResponseEntity<DomainResponse> post(@RequestBody User_Detail user){
 		System.out.println("-------------------------------------reached into controller1--------------------------------------------");
 		userDao.add(user);
@@ -37,7 +37,39 @@ public class UserController {
 	}
 	
 	@RequestMapping("/all")
-	public ResponseEntity<List<User_Detail>> get(){
+	public ResponseEntity<List<User_Detail>> getList(){
 		return new ResponseEntity<List<User_Detail>>(userDao.userlist(),HttpStatus.OK);
+	}
+	
+	@RequestMapping("/delete/{id}")
+	public ResponseEntity<DomainResponse> deleteUserById(@PathVariable int id) {
+		userDao.delete(id); 
+		return new ResponseEntity<DomainResponse>(new DomainResponse("deleted the data",100), HttpStatus.OK);
+
+	}
+	
+	@PostMapping("/update/{id}")
+	public ResponseEntity<DomainResponse> updateUser(@RequestBody User_Detail user,@PathVariable int id){
+		
+		User_Detail currentUser = userDao.getUserDetail(id);
+		currentUser.setUserId(user.getUserId());
+		currentUser.setAddress(user.getAddress());
+		currentUser.setActive(user.isActive());
+		currentUser.setCity(user.getCity());
+		currentUser.setEmail(user.getEmail());
+		currentUser.setGender(user.getGender());
+		currentUser.setIsOnline(user.getIsOnline());
+		currentUser.setName(user.getName());
+		currentUser.setPassword(user.getPassword());
+		currentUser.setPhoneNo(user.getPhoneNo());
+		currentUser.setProfile(user.getProfile());
+		currentUser.setRole(user.getRole());
+		currentUser.setState(user.getState());
+		currentUser.setUserName(user.getUserName());
+		currentUser.setStatus(user.getStatus());
+		currentUser.setBirthDate(user.getBirthDate());
+		userDao.update(currentUser);
+		
+		return new ResponseEntity<DomainResponse> (new DomainResponse("user recieved the data",100), HttpStatus.OK);
 	}
 }
